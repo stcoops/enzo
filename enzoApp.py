0ee39cf541b178c9d9d9723166bf33901471c067
S <samtonycooper@gmail.com>
2025-06-27 17:55:54 +0100

    update

--- enzoApp.py
+++ enzoApp.py
@@ -7,7 +7,7 @@ from textual.events import Event
 from textual import on 
 from textual.widget import Widget
 from rich.progress import Progress
-import time
+import time, asyncio
 """
 WIDGETS
 """
@@ -36,8 +36,7 @@ class LoadingScreen(Screen):
     
     def compose(self) -> ComposeResult:
         yield Container(
-            Label("Loading babalabs..", id = "CurrentTask"),
-            ProgressBar(total = 10)
+            Label("Loading: "+  NAME, id = "CurrentTask")
         )
 
 class DashboardScreen(Screen):
@@ -61,41 +60,65 @@ class EnzoApp(App):
     
     #BINDINGS
     CSS_PATH = "styles.tcss"
-    #SCREENS = {"LoadingScreen": LoadingScreen, "MainDashboard": DashboardScreen}
+    SCREENS = {"LoadingScreen": LoadingScreen, "MainDashboard": DashboardScreen}
 
     def __init__(self) -> None:
         super().__init__()
         
 
     def compose(self) -> ComposeResult:
-        yield Label("Hello World")
+        yield Center(
+            VerticalGroup(
+                HorizontalGroup(
+                    Button("Welcome", id="welcome_button"),
+                    Button("Loading", id="loading_button"),
+                    Button("Dashboard", id="dashboard_button")
+                ),
+                Label("Enzo App - Alpha 1.0.1"),
+                Label("This is a demo app for Enzo."),
+                Label("Click the buttons to navigate.")
+            ),
+        Label("Hello World"),
+        ProgressBar(id="progress_bar", total=100))
         
-    def on_mount(self) -> None:
-        self.install_screen(WelcomeScreen(), "WelcomeScreen")
-        self.install_screen(LoadingScreen(), "LoadingScreen")
-        self.push_screen("LoadingScreen")
+        
+    #def on_mount(self) -> None:
+        #self.install_screen(WelcomeScreen(), "WelcomeScreen")
+        #self.install_screen(LoadingScreen(), "LoadingScreen")
+        #self.push_screen("LoadingScreen")
 
 """
 MAIN CLASS
 """
-class z():
-    def __init__(self):
-        super().__init__()
-        self.app = EnzoApp()
-        self.app.run()
-        #self.app.switch_screen("LoadingScreen")
-        time.sleep(0.5)
-        self.Load()
-    
-    def Load(self) -> None:
-        i = 0
-        while i != 100:
-            time.sleep(0.03)
-            i += 1
-            self.app.query_one(ProgressBar).advance(1)
+def main(loop) -> None:
+    """Main function to run the EnzoApp."""
+    # Create an instance of the EnzoApp and run it.
+    # This will initialize the app and display the welcome screen.
+    ENZO = EnzoApp()
+    ENZO.run(loop=loop)
+            #self.app.switch_screen("LoadingScreen")
+    time.sleep(0.5)
+        
+    ENZO.push_screen(LoadingScreen())
+    i = 0
+    while i != 100:
+        time.sleep(0.03)
+        i += 1
+                
+        ENZO.query_one(ProgressBar).update(progress=i, total=100)
         print(i)
 
+if __name__ == '__main__':
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        (main(loop=loop))
+    except KeyboardInterrupt:
+        pass
 
-if __name__ == "__main__":
-    Enzo = z()
+
+
+
+
+
 
